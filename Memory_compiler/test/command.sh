5b43db96e5c67c7b86d64b9bd9ed90f4df74a82e
zero <prithamhsuvarna@gmail.com>
2024-10-11 22:14:57 +0530

    changing path setup in shell script

--- Memory_compiler/test/command.sh
+++ Memory_compiler/test/command.sh
@@ -1,4 +1,7 @@
-cd ..
-source ~/OpenRAM/setpaths.sh
-cd test
+#!/bin/bash
+
+export OPENRAM_HOME="~/OpenRAM/compiler"
+export OPENRAM_TECH="~/OpenRAM/technology"
+export PDK_ROOT="$HOME/gf/pdk"
+export PYTHONPATH=$OPENRAM_HOME
 python3 $OPENRAM_HOME/../sram_compiler.py SRAM_32x128_1rw-cfg.py
