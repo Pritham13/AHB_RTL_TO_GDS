5cefc9e35a5db8ebafe3f969ca6caf9b4dbbf17a
Pritham13 <prithamhsuvarna@gmail.com>
2024-10-12 11:37:31 +0530

    changing the IO naming conventions

--- AHB_manager/rtl/AHB_manager.v
+++ AHB_manager/rtl/AHB_manager.v
@@ -1,26 +1,26 @@
 module AHB_manager (
-  input  i_HCLK,
-  input  i_HRESETn,
+  input  HCLK,
+  input  HRESETn,
   
   // Master Outputs
-  output reg  [31:0] o_HADDR,
-  output reg  [2:0]  o_HBURST,
-  output reg         o_HMASTLOCK,
-  output reg  [3:0]  o_HPROT,
-  output reg  [2:0]  o_HSIZE,
-  output reg  [1:0]  o_HTRANS,
-  output reg  [31:0] o_HWDATA,
-  output reg         o_HWRITE,
+  output reg  [31:0] HADDR,
+  output reg  [2:0]  HBURST,
+  output reg         HMASTLOCK,
+  output reg  [3:0]  HPROT,
+  output reg  [2:0]  HSIZE,
+  output reg  [1:0]  HTRANS,
+  output reg  [31:0] HWDATA,
+  output reg         HWRITE,
   
   // Master Inputs
-  input   [31:0] i_HRDATA,
-  input          i_HREADY,
-  input   [1:0]  i_HRESP,
+  input   [31:0] HRDATA,
+  input          HREADY,
+  input   [1:0]  HRESP,
   
   // Arbiter Signals
-  output reg     o_HBUSREQ,
-  input          i_HGRANT,
-  output reg     o_HLOCK
+  output reg     HBUSREQ,
+  input          HGRANT,
+  output reg     HLOCK
 ); 
 localparam ADDR_NUM = 3 ;
 localparam  s_addr ,
@@ -31,27 +31,27 @@ reg [31:0] addr_mem [ADDR_NUM - 1 : 0] ;
 reg [3:0] r_ptr_addr ;
 
 //////////////////arbriter Request driver////////////// 
-always @(i_HCLK) begin 
-  if(!i_HGRANT)begin 
-    o_HBUSREQ <= 1 ;
+always @(HCLK) begin 
+  if(!HGRANT)begin 
+    HBUSREQ <= 1 ;
     r_ptr_addr <= 0 ;
   end 
   else 
-    o_HBUSREQ <= 0 ; //NOTE: needs to be checked
+    HBUSREQ <= 0 ; //NOTE: needs to be checked
 
 /////////////////////addr driving blocks///////////// 
 
-always @(i_HCLK) begin 
-  if (i_HREADY && i_HGRANT) begin 
-    r_data_buffer <= i_HRDATA ; 
-    o_HADDR <= addr_mem[r_ptr_addr] ;
+always @(HCLK) begin 
+  if (HREADY && HGRANT) begin 
+    r_data_buffer <= HRDATA ; 
+    HADDR <= addr_mem[r_ptr_addr] ;
     r_ptr_addr <= r_ptr_addr + 1 ;
   end
 end
 
 ////////////////////data driving blocks///////////// 
 
-always @(i_HCLK) begin 
-  if (i_HREADY && i_HGRANT)
-    o_HWDATA  <= r_data_buffer ;
+always @(HCLK) begin 
+  if (HREADY && HGRANT)
+    HWDATA  <= r_data_buffer ;
 end
